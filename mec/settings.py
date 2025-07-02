1680f72581d14e8bc0bdff1e4fb266eee557e7af
israr-ax <israrmemon211@gmail.com>
2025-07-03 02:09:02 +0500

    Initial Commit
    Initial Commit

--- mec/settings.py
+++ mec/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-ht4y&1_$!700uy-hzkt=hb0b-+#qc8o-4i%$c$7i*iu&6-wjf9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIRS = [os.path.join(BASE_DIR,'shop/static')]
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
